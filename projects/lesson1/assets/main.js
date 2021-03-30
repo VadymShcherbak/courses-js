9778c8858e85f6af22cb977865222b1975ab8cb2
VadymShcherbak <vadymshcherback92@gmail.com>
2021-03-30 15:31:49 +0300

    learn js

--- projects/lesson1/assets/main.js
+++ projects/lesson1/assets/main.js
@@ -1,8 +1,8 @@
 console.log('====Task-1=====');
 
 function strToArray(str) {
-	let result = str.split(' ');
-	return result; 
+
+	return str.split(' ');
 }
 
 console.log(strToArray('Lorem ipsum dolor sit amet consectetur'));
@@ -10,8 +10,8 @@ console.log(strToArray('Lorem ipsum dolor sit amet consectetur'));
 console.log('====Task-2=====');
 
 function numSymbol(str) {
-	let result = str.length;
-	return result;
+
+	return str.length;
 }
 
 console.log(numSymbol('Lorem ipsum dolor sit amet consectetur'));
@@ -19,8 +19,8 @@ console.log(numSymbol('Lorem ipsum dolor sit amet consectetur'));
 console.log('====Task-3=====');
 
 function insertSpaceWords(str, delimiter) {
-	let result = str.split(' ').join(delimiter);
-	return result;
+
+	return str.split(' ').join(delimiter);
 }
 
 console.log(insertSpaceWords('Lorem ipsum dolor sit amet consectetur', '-'));
@@ -28,8 +28,13 @@ console.log(insertSpaceWords('Lorem ipsum dolor sit amet consectetur', '-'));
 console.log('====Task-4=====');
 
 function strToUpperCase(str) {
-	let result = str.toUpperCase();
-	return result;
+	let result = str.split(' ');
+
+	for (let i = 0; i < result.length; i++) {
+		result[i] = result[i].charAt(0).toUpperCase() + result[i].slice(1).toLowerCase();
+	}
+
+	return result.join(' ');
 }
 
 console.log(strToUpperCase('Lorem ipsum dolor sit amet consectetur'));
@@ -37,8 +42,8 @@ console.log(strToUpperCase('Lorem ipsum dolor sit amet consectetur'));
 console.log('====Task-5=====');
 
 function strCompare(str1, str2) {
-	let result = (str1.toUpperCase() === str2.toUpperCase())
-	return result;
+
+	return str1.toUpperCase() === str2.toUpperCase();
 }
 
 console.log(strCompare('Lorem ipsum dolor sit amet consectetur', 'Lorem Ipsum dolor sit amet consectetur'));
@@ -46,16 +51,12 @@ console.log(strCompare('Lorem ipsum dolor sit amet consectetur', 'Lorem Ipsum do
 console.log('====Task-6=====');
 
 function searchStrInStr(str1, str2) {
-	str1 = str1.toUpperCase();
-	str2 = str2.toUpperCase();
-	let result = str1.split(' ').indexOf(str2);
+	return str1.toUpperCase().split(' ').indexOf(str2.toUpperCase()) != -1;
 	// if (str2 != -1) {
 	// 	result = true;
 	// } else {
 	// 	result = false;
 	// }
-	result = str2 != -1 ? true : false;
-	return result;
 }
 
 console.log(searchStrInStr('Lorem ipsum dolor sit amet consectetur', 'Ipsum'));
@@ -63,37 +64,42 @@ console.log(searchStrInStr('Lorem ipsum dolor sit amet consectetur', 'Ipsum'));
 console.log('====Task-7=====');
 
 function strToCamelCase(str) {
-	return str.toLowerCase().replace(/(?:^|\s)[a-z]/g, function(m) {
-		return m.toUpperCase().replace(/\s+/g, "");
-	});
+	let result = str.split(' ');
+
+	for (let i = 0; i < result.length; i++) {
+		result[i] = result[i].charAt(0).toUpperCase() + result[i].slice(1).toLowerCase();	
+	}
+
+	return result.join('').charAt(0).toLowerCase() + result.join('').slice(1);
 }
 
-console.log(strToCamelCase('ipsum dolor sit amet consectetur'));
+console.log(strToCamelCase('ipsum dolor sit amet'));
 
 console.log('====Task-8=====');
 
-function insertSpaceWords(str, delimiter) {
-	let result = str.split(' ').join(delimiter);
-	return result;
+function insertWords(str, delimiter) {
+
+	return str.split(' ').join(delimiter);
 }
 
-console.log(insertSpaceWords('Lorem ipsum dolor sit amet consectetur', '_'));
+console.log(insertWords('Lorem ipsum dolor sit amet consectetur', '_'));
 
 console.log('====Task-9=====');
 
 function repeatWord(word, num) {
+
 	for (let i = 0; i < num; i++) {
 		console.log(word);
 	}
 }
 
-console.log(repeatWord('lorem', 5));
+repeatWord('lorem', 5);
 
 console.log('====Task-10=====');
 
 function partOfStr(str, symbol) {
-	let result = str.substr(0, str.indexOf(symbol));
-	return result;
+
+	return str.substr(0, str.indexOf(symbol));
 }
 
 console.log(partOfStr('Lorem ipsum dolor - sit amet consectetur', '-'));
@@ -101,6 +107,7 @@ console.log(partOfStr('Lorem ipsum dolor - sit amet consectetur', '-'));
 console.log('====Task-11=====');
 
 function shareSymbol(symbol, num) {
+
 	var res = [];
 	for(let i = 0 ; i < symbol.length; i += num) {
     res.push(symbol.substr(i, num));
@@ -113,8 +120,8 @@ console.log(shareSymbol('consectetur', 3));
 console.log('====Task-12=====');
 
 function returnLetter(str) {
-	let result = str.split(' ').join('').length;
-	return result;
+
+	return str.split(' ').join('').length;
 }
 
 console.log(returnLetter('amet consectetur'));
@@ -122,8 +129,118 @@ console.log(returnLetter('amet consectetur'));
 console.log('====Task-13=====');
 
 function sortLet(str) {
-	let result = str.split(' ').sort().join('');
-	return result;
+
+	return str.split('').sort().join('');
 }
 
 console.log(sortLet('consectetur'));
+
+console.log('====Task-14====='); 
+
+function delRepeatWord(str) {
+
+	let array = str.split(',');
+	let result = [];
+
+	for (let i = 0; i < array.length; i++) {
+		if (result.indexOf(array[i]) === -1) {
+			result.push(array[i]);
+		}
+	}
+
+	return result = result.join('');
+}
+
+console.log(delRepeatWord('вишня, груша, слива, груша'));
+
+console.log('====Task-15=====');
+
+function strToLowerCase(str) {
+
+	let result = str.split(' ');
+
+	for (let i = 0; i < result.length; i++) {
+		result[i] = result[i].charAt(0).toLowerCase() + result[i].slice(1);
+	}
+
+	return result.join(' ');
+}
+
+console.log(strToLowerCase('Lorem ipsum dolor sit amet consectetur'));
+
+console.log('====Task-16=====');
+
+function typeValue(str) {
+	
+	return typeof(str);
+}
+
+console.log(typeValue('lorem ipsum dolor'));
+
+console.log('====Task-17=====');
+
+function longStr(array) {
+	
+	let result = '';
+
+	for (let i = 0; i < array.length; i++) {
+		if (array[i].length > result.length) {
+			result = array[i];
+		}
+	}
+
+	return result;
+}
+
+console.log(longStr(['lorem', 'loremloremloremlorem', 'ipsum']));
+
+console.log('====Task-18=====');
+
+function longWord(str) {
+	
+	let array = str.split(' ');
+	let result = '';
+
+	for (let i = 0; i < array.length; i++) {
+		if (array[i].length > result.length) {
+			result = array[i];
+		}
+	}
+
+	return result;
+}
+
+console.log(longWord('lorem loremipsumdolor ipsum'));
+
+console.log('====Task-19====='); 
+
+function wordSpace(str) {
+	
+	let array = str.split(' ');
+	let result = 0;
+
+	for (let i = 0; i <= array.length; i++) {
+		result = i;
+	}
+
+	return result;
+}
+
+console.log(wordSpace('lorem ipsum dolor sit amet'));
+
+console.log('====Task-20====='); 
+
+function changeDate(date) {
+	
+	let array = date.split('-');
+	let result = [];
+
+	result.push(array[2]);
+	result.push(array[1]);
+	result.push(array[0]);
+
+	return result.join('-');
+}
+
+console.log(changeDate('31-12-2030'));
+
