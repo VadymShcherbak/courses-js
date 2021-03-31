a6dfaaba2fd4d2ae0253b40ea4bece57ef3b233c
VadymShcherbak <vadymshcherback92@gmail.com>
2021-03-31 17:04:07 +0300

    learn js day 3

--- projects/lesson1/assets/main.js
+++ projects/lesson1/assets/main.js
@@ -1,30 +1,34 @@
+//==========1
 console.log('====Task-1=====');
 
 function strToArray(str) {
-
 	return str.split(' ');
 }
 
 console.log(strToArray('Lorem ipsum dolor sit amet consectetur'));
 
+
+//==========2
 console.log('====Task-2=====');
 
 function numSymbol(str) {
-
 	return str.length;
 }
 
 console.log(numSymbol('Lorem ipsum dolor sit amet consectetur'));
 
+
+//==========3
 console.log('====Task-3=====');
 
 function insertSpaceWords(str, delimiter) {
-
 	return str.split(' ').join(delimiter);
 }
 
 console.log(insertSpaceWords('Lorem ipsum dolor sit amet consectetur', '-'));
 
+
+//==========4
 console.log('====Task-4=====');
 
 function strToUpperCase(str) {
@@ -39,15 +43,18 @@ function strToUpperCase(str) {
 
 console.log(strToUpperCase('Lorem ipsum dolor sit amet consectetur'));
 
+
+//==========5
 console.log('====Task-5=====');
 
 function strCompare(str1, str2) {
-
 	return str1.toUpperCase() === str2.toUpperCase();
 }
 
 console.log(strCompare('Lorem ipsum dolor sit amet consectetur', 'Lorem Ipsum dolor sit amet consectetur'));
 
+
+//==========6
 console.log('====Task-6=====');
 
 function searchStrInStr(str1, str2) {
@@ -61,6 +68,8 @@ function searchStrInStr(str1, str2) {
 
 console.log(searchStrInStr('Lorem ipsum dolor sit amet consectetur', 'Ipsum'));
 
+
+//==========7
 console.log('====Task-7=====');
 
 function strToCamelCase(str) {
@@ -75,19 +84,21 @@ function strToCamelCase(str) {
 
 console.log(strToCamelCase('ipsum dolor sit amet'));
 
+
+//==========8
 console.log('====Task-8=====');
 
 function insertWords(str, delimiter) {
-
 	return str.split(' ').join(delimiter);
 }
 
 console.log(insertWords('Lorem ipsum dolor sit amet consectetur', '_'));
 
+
+//==========9
 console.log('====Task-9=====');
 
 function repeatWord(word, num) {
-
 	for (let i = 0; i < num; i++) {
 		console.log(word);
 	}
@@ -95,20 +106,23 @@ function repeatWord(word, num) {
 
 repeatWord('lorem', 5);
 
+
+//==========10
 console.log('====Task-10=====');
 
 function partOfStr(str, symbol) {
-
 	return str.substr(0, str.indexOf(symbol));
 }
 
 console.log(partOfStr('Lorem ipsum dolor - sit amet consectetur', '-'));
 
+
+//==========11
 console.log('====Task-11=====');
 
 function shareSymbol(symbol, num) {
-
 	var res = [];
+
 	for(let i = 0 ; i < symbol.length; i += num) {
     res.push(symbol.substr(i, num));
 	}
@@ -117,24 +131,28 @@ function shareSymbol(symbol, num) {
 
 console.log(shareSymbol('consectetur', 3));
 
+
+//==========12
 console.log('====Task-12=====');
 
 function returnLetter(str) {
-
 	return str.split(' ').join('').length;
 }
 
 console.log(returnLetter('amet consectetur'));
 
+
+//==========13
 console.log('====Task-13=====');
 
 function sortLet(str) {
-
 	return str.split('').sort().join('');
 }
 
 console.log(sortLet('consectetur'));
 
+
+//==========14
 console.log('====Task-14====='); 
 
 function delRepeatWord(str) {
@@ -153,10 +171,11 @@ function delRepeatWord(str) {
 
 console.log(delRepeatWord('вишня, груша, слива, груша'));
 
+
+//==========15
 console.log('====Task-15=====');
 
 function strToLowerCase(str) {
-
 	let result = str.split(' ');
 
 	for (let i = 0; i < result.length; i++) {
@@ -168,19 +187,21 @@ function strToLowerCase(str) {
 
 console.log(strToLowerCase('Lorem ipsum dolor sit amet consectetur'));
 
+
+//==========16
 console.log('====Task-16=====');
 
 function typeValue(str) {
-	
 	return typeof(str);
 }
 
 console.log(typeValue('lorem ipsum dolor'));
 
+
+//==========17
 console.log('====Task-17=====');
 
 function longStr(array) {
-	
 	let result = '';
 
 	for (let i = 0; i < array.length; i++) {
@@ -194,10 +215,11 @@ function longStr(array) {
 
 console.log(longStr(['lorem', 'loremloremloremlorem', 'ipsum']));
 
+
+//==========18
 console.log('====Task-18=====');
 
 function longWord(str) {
-	
 	let array = str.split(' ');
 	let result = '';
 
@@ -212,10 +234,11 @@ function longWord(str) {
 
 console.log(longWord('lorem loremipsumdolor ipsum'));
 
+
+//==========19
 console.log('====Task-19====='); 
 
 function wordSpace(str) {
-	
 	let array = str.split(' ');
 	let result = 0;
 
@@ -228,10 +251,11 @@ function wordSpace(str) {
 
 console.log(wordSpace('lorem ipsum dolor sit amet'));
 
+
+//==========20
 console.log('====Task-20====='); 
 
 function changeDate(date) {
-	
 	let array = date.split('-');
 	let result = [];
 
@@ -244,3 +268,80 @@ function changeDate(date) {
 
 console.log(changeDate('31-12-2030'));
 
+
+//==========21
+console.log('====Task-21====='); 
+
+function twoArrayInOne(str1, str2) {
+	let array1 = str1.split(' ');
+	let array2 = str2.split(' ');
+	let result = array1.concat(array2);
+
+	return result
+}
+
+console.log(twoArrayInOne('lorem ipsum', 'dolor sit'));
+
+
+//==========22
+console.log('====Task-22====='); 
+
+function addLastElement(str) {
+	let array = str.split(' ');
+	array.push('amet')
+
+	return array
+}
+
+console.log(addLastElement('lorem ipsum dolor sit'));
+
+
+//==========23
+console.log('====Task-23====='); 
+
+function reverseArray(array) {
+	return array.split(' ').reverse();
+}
+
+console.log(reverseArray('lorem ipsum dolor sit'));
+
+
+//==========24
+console.log('====Task-24====='); 
+
+function deleteElement(array) {
+	let result = array.split(' ');
+	let index = result.indexOf('dolor');
+
+	if (index > -1) {
+		result.splice(index, 1);
+	}
+
+	return result
+}
+
+console.log(deleteElement('lorem ipsum dolor sit'));
+
+
+//==========25
+console.log('====Task-25====='); 
+
+// function calculateAge(year) {
+// 	return 2021 - year;
+// }
+
+// console.log(calculateAge(1992));
+
+function myBirthDay() {
+	let date1 = new Date('1992.08.14');
+	let date2 = new Date();
+	let result = date2.getFullYear() - date1.getFullYear();
+
+	if (date2.getMonth() < date1.getMonth()) {
+		result --;
+	}
+
+	return result;
+}
+
+console.log(myBirthDay());
---
+++ projects/lesson1/lesson0.js
@@ -0,0 +1,215 @@
+// const fullAge = 28
+// const birthYear = 1992
+// const currentYear = 2021
+
+// const isFullAge = (currentYear - birthYear) >= fullAge // 28 >= 27 true 
+// console.log(isFullAge);
+
+// const courseStatus = 'fail' // ready, fail ,panding
+
+// if (courseStatus === 'ready') {
+// 	console.log('курс уже готов');
+// }
+// else if (courseStatus === 'panding') {
+// 	console.log('курс в процессе розробки');
+// }
+// else {
+// 	console.log('курс не получился');
+// }
+// const isReady = true;
+
+// if (isReady) {
+// 	console.log('all done');
+// }
+// else {
+// 	console.log('all not done');
+// }
+
+// function calculateAge(year) {
+// 	return 2021 - year
+// }
+
+// // const myAge = calculateAge(1992)
+// // console.log(calculateAge(1992));
+// // console.log(calculateAge(2002));
+// // console.log(calculateAge(1997));
+
+// function logInfoAbout(name, year) {
+// 	const age = calculateAge(year);
+
+// 	if (age > 0) {
+// 		console.log('Человек по имени' + name + 'сейчас имеет возраст' + age);
+// 	} else {
+// 		console.log('Вообщето ето уже будущее');
+// 	}
+// }
+
+// logInfoAbout('Вадим', 1992 );
+// logInfoAbout('Елена', 2022 );
+
+// let cars = ['mazda', 'mercedes', 'ford']
+// let cars = new Array('mazda', 'mercedes', 'ford')
+// console.log(cars.length)
+// console.log(cars[0])
+// console.log(cars[1])
+// console.log(cars[2])
+
+// cars[0] = 'Porsche'
+// cars[3] = 'mazda'
+// console.log(cars);
+
+// ЦИКЛ
+// let cars = ['mazda', 'mercedes', 'ford', 'porsche']
+
+// for (let i = 0; i < cars.length; i++) {
+// 	let car = cars[i]
+// 	console.log(car);
+// }
+
+// Objects
+// const person = {
+// 	firstName: 'Vadym',
+// 	lastName: 'Shcherbak',
+// 	year: 1992,
+// 	languages: ['Ukr', 'Ru', 'En'],
+// 	hasWife: false,
+// 	greet: function () {
+// 		console.log('greet from person');
+// 	}
+// }
+
+// console.log(person.firstName);
+// person.greet()
+
+// ======== NUMBER ========== //
+
+
+// const num = 42 // integer
+// const float = 42.42 // float
+// const pow = 10e3
+
+// console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER);
+// console.log('Math.pow 53', Math.pow(2, 53,) - 1);
+// console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER);
+// console.log('MAX_VALUE', Number.MAX_VALUE);
+// console.log('MAX_VALUE', Number.MIN_VALUE);
+// console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY);
+// console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY);
+// console.log('2 / 0', 2 / 0);
+// console.log(Number.NaN); // not a number
+// console.log(typeof NaN);
+// const weird = 2 / undefined
+// console.log(Number.isNaN(weird));
+// console.log(Number.isNaN(42));
+// console.log(Number.isFinite(Infinity));
+// console.log(Number.isFinite(42));
+
+// const stringInt = '40'
+// const stringFloat = '40.42'
+// console.log(Number.parseInt(stringInt) + 2);
+// console.log(Number(stringInt) + 2);
+// console.log(+stringInt + 2);
+
+// console.log(parseFloat(stringFloat) + 2);
+// console.log(+stringFloat + 2);
+
+// console.log(0.4 + 0.2);
+// console.log(+(0.4 + 0.2).toFixed(1));
+// console.log(parseFloat((0.4 + 0.2).toFixed(1)));
+
+// СТРОКИ
+
+// const myname = 'Vadym'
+// const age = 26 
+
+// function getAge() {
+// 	return age
+// }
+
+// // const output = 'Hello, my name is ' + myname + ' i have ' + age + ' years old'
+// const output = `Hello, my name is ${myname} i have ${getAge()} years old`
+// console.log(output);
+
+// const myname = 'Vadym'
+// console.log(myname.length);
+// console.log(myname.toUpperCase());
+// console.log(myname.toLocaleLowerCase());
+// console.log(myname.charAt(2));
+// console.log(myname.indexOf('!'));
+
+// function logPerson(s, name, age) {
+// 	if (age < 0) {
+// 		age = 'Еще не родился'
+// 	}
+// 	return `${s[0]}${name}${s[1]}${age}${s[2]}`
+// }
+
+// const personName = 'Вадим'
+// const personName2 = 'Максим'
+// const personAge = 28
+// const personAge2 = -10
+
+// const output = logPerson `Имя: ${personName}, Возраст: ${personAge}!`
+// const output2 = logPerson `Имя: ${personName2}, Возраст: ${personAge2}!`
+
+// console.log(output);
+// console.log(output2);
+
+// ФУНКИИ 
+// Function Declaration
+// function greet(name) {
+// 	console.log('Привет - ', name);
+// }
+
+//Function Expression
+// const greet2 = function greet2(name) {
+// 	console.log('Привет 2 -', name);
+// }
+
+// greet('Лена')
+// greet2('Лена')
+
+//АНОНИМНЫЕ ФУНКЦИИ
+// let counter = 0
+// const interval = setInterval(function() {
+// 	if (counter === 5) {
+// 		clearInterval(interval)
+// 	} else {
+// 		console.log(++counter);
+// 	}
+// }, 1000)
+
+//СТРЕЛОЧНЫЕ ФУНКЦИИ
+// function greet(name) {
+// 	console.log('Привет - ', name);
+// }
+
+// const arrow = (name) => {
+// 	console.log('Привет - ', name);
+// }
+
+// const arrow2 = name => console.log('Привет - ', name);
+
+// arrow2('Vadym')
+
+// const pow2 = num => num ** 2
+
+// // console.log(pow2(5));
+
+// //ПАРАМЕТРИ ПО УМОЛЧАНИЮ
+// const sum = (a = 40, b = a * 2) => a + b
+// console.log(sum(41, 4));
+// console.log(sum());
+
+// function sumAll(...all) {
+// 	let result = 0
+// 	for (let num of all) {
+// 		result += num
+// 	}
+// 	return result
+// }
+
+// const res = sumAll(1, 2, 3, 4, 5)
+// console.log(res);
+
+// МАСИВЫ
