dbf8991d6afcb89590f80ede8423f0e79ae9c541
VadymShcherbak <vadymshcherback92@gmail.com>
2021-04-07 12:25:43 +0300

    lesson 3 slider

--- projects/lesson3/assets/js/main.js
+++ projects/lesson3/assets/js/main.js
@@ -1,4 +1,4 @@
-
+//=========== TASK-1 ==========
 $('.products').each(function() {
 	let $products = $(this);
 	let $tabMenu = $products.find('.tab-menu > li');
@@ -19,7 +19,7 @@ $('.products').each(function() {
 	});
 });
 
-//============ TASK - 2 ========
+//============ TASK - 2, 3 ========
 $('.accordion').each(function() {
 	let $sectionAccordion = $(this);
 	let $accordionItem = $sectionAccordion.find('.accordion-wrapper > div');
@@ -33,7 +33,50 @@ $('.accordion').each(function() {
 		$accordionItem.on('click', function() {
 			let $accOneOpen = $(this);
 			$accOneOpen.siblings().removeClass('accordion-item-open');
-			$accOneOpen.addClass('accordion-item-open');
+			$accOneOpen.toggleClass('accordion-item-open');
 		});
 	}
+});
+
+//============== TASK - 4 ============
+function sliderNav(element, sectionSlider) {
+	let $activeSlider = sectionSlider.find('.slider-wrapper .active');
+	let $sliderItem = sectionSlider.find('.slider-wrapper > div');
+	let $dataNav;
+	
+	$activeSlider.removeClass('active');
+
+	if ('prev' === element.data('sl-nav')) {
+		$dataNav = $activeSlider.prev(); 
+
+		if ($dataNav.length) {
+			$dataNav.addClass('active');
+		} else {
+			$sliderItem.last().addClass('active');
+		}
+	} else if ('next' === element.data('sl-nav')) {
+		$dataNav = $activeSlider.next();
+
+		if ($dataNav.length) {
+			$dataNav.addClass('active');
+		} else {
+			$sliderItem.first().addClass('active');
+		}
+	}
+}
+
+$('.slider').each(function () {
+	let $mainSlider = $(this);
+	let $prevBtn = $mainSlider.find('.prev-btn');
+	let $nextBtn = $mainSlider.find('.next-btn');
+
+	$prevBtn.on('click', function(e) {
+		e.preventDefault();
+		sliderNav($(this), $mainSlider);
+	});
+
+	$nextBtn.on('click', function(e) {
+		e.preventDefault();
+		sliderNav($(this), $mainSlider);
+	});
 });
